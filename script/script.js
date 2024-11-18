53e3448258f3560f3de5c5d4f3def40ed3368703
Chris-suk <yebonsuk@gmail.com>
2024-11-17 20:46:04 -0700

    Create a loop

--- script/script.js
+++ script/script.js
@@ -3,20 +3,19 @@ const fYear = document.getElementById("copyright");
 const d = new Date();
 fYear.innerText = d.getFullYear();
 
+
+
 //greets 
     const currentHour = new Date (). getHours ();
     const greetingElement = document.getElementById('greetings');
     if (currentHour < 12) {
         greetingElement.textContent = 'Good Morning!';
-
         greetingElement.classList.add('morning');
       } else if (currentHour >= 12 && currentHour < 17) {
         greetingElement.textContent = 'Good Afternoon!';
-
         greetingElement.classList.add('afternoon');
       } else {
         greetingElement.textContent = 'Good Evening!';
-
         greetingElement.classList.add('evening');
       }
 
@@ -34,3 +33,17 @@ alertBtn.addEventListener ('mouseover', () => {
 alertBtn.addEventListener('mouseout', () => {
     alertBtn.textContent = "Click me!";
 });
+
+//loop
+const olElement = document.getElementById('numbers'); 
+for (let i = 1; i <= 12; i++) {
+  const listItem = document.createElement('li');
+
+  if (i % 2 === 0) {
+    listItem.textContent = 'Even'; 
+  } else {
+    listItem.textContent = 'Odd'; 
+  }
+  
+  olElement.appendChild(listItem);
+}
